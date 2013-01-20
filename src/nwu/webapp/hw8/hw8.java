cfaef650c7bb10581caa1aa48ab99010664e174f
NingzhiWu <wuningzhi@gmail.com>
2013-01-19 22:28:10 -0500

    test

--- src/nwu/webapp/hw8/hw8.java
+++ src/nwu/webapp/hw8/hw8.java
@@ -29,7 +29,17 @@ public class hw8 extends HttpServlet {
 		private FavoriteDAO favoriteDAO;
 		private UserDAO userDAO;
 		
-		
+		public void init() throws ServletException {
+			String jdbcDriverName = getInitParameter("jdbcDriver");
+			String jdbcURL        = getInitParameter("jdbcURL");
+			
+			try {
+				userDAO  = new UserDAO(jdbcDriverName,jdbcURL,"nwu_user");
+				favoriteDAO   = new FavoriteDAO(jdbcDriverName,jdbcURL,"nwu_favorite");
+			} catch (MyDAOException e) {
+				throw new ServletException(e);
+			}
+		}
 
 		public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	        HttpSession session = request.getSession();
